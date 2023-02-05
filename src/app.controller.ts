3fc338dc8b9512906b93a8764966223618c1f88a
evgeny-sav <ysauchanka@aristeksystems.com>
2023-02-05 17:09:45 +0100

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import {Controller, Get, HttpCode, HttpStatus} from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  healthcheck(): string {
+    return this.appService.getHealth();
+  }
+}
